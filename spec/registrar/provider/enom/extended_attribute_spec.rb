08e2612e254893cc9e77daedd978e1b7cb25e109
Anthony Eden <anthonyeden@gmail.com>
2011-09-09 00:46:20 +0800

    test Enom extended attributes

--- spec/registrar/provider/enom/extended_attribute_spec.rb
+++ spec/registrar/provider/enom/extended_attribute_spec.rb
@@ -3,27 +3,35 @@ require 'registrar/provider/enom/extended_attribute'
 
 describe Registrar::Provider::Enom::ExtendedAttribute do
 
-  context "for a .ca extended attribute" do
-    let(:extended_attribute) { Registrar::ExtendedAttribute.new('ca', :"Legal Type", :"Canadian Citizen") }
-    let(:enom_extended_attribute) { Registrar::Provider::Enom::ExtendedAttribute.new(extended_attribute) }
+  let(:enom_extended_attribute) { Registrar::Provider::Enom::ExtendedAttribute.new(extended_attribute) }
 
+  shared_examples "an extended attribute" do
     it "has a name" do
-      enom_extended_attribute.name.should eq('cira_legal_type')
+      enom_extended_attribute.name.should eq name
     end
     it "has a value" do
-      enom_extended_attribute.value.should eq('CCT')
+      enom_extended_attribute.value.should eq value
     end
+  end
+
+  context "for a .ca extended attribute" do
+    let(:extended_attribute) { Registrar::ExtendedAttribute.new('ca', :"Legal Type", :"Canadian Citizen") }
+    let(:name) { 'cira_legal_type' }
+    let(:value) { 'CCT' }
+    it_behaves_like "an extended attribute"
   end
 
   context "for a .us extended attribute" do
     let(:extended_attribute) { Registrar::ExtendedAttribute.new('us', :"Nexus", :"US Citizen") }
-    let(:enom_extended_attribute) { Registrar::Provider::Enom::ExtendedAttribute.new(extended_attribute) }
-
-    it "has a name" do
-      enom_extended_attribute.name.should eq('us_nexus')
-    end
-    it "has a value" do
-      enom_extended_attribute.value.should eq('C11')
-    end
+    let(:name)  { 'us_nexus' }
+    let(:value) { 'C11' }
+    it_behaves_like "an extended attribute"
+  end
+  
+  context "for a .IO extended attribute" do
+    let(:extended_attribute) { Registrar::ExtendedAttribute.new('io', :"Renewal Agreement", :"Yes") } 
+    let(:name) { 'io_agreedelete' }
+    let(:value) { 'YES' }
+    it_behaves_like "an extended attribute"
   end
 end
